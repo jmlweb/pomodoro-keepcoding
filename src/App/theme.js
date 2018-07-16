99f2e7293d2d8bb269ae1d4371e2e1f7226beaa0
José Manuel Lucas <josemanuel@jmlweb.es>
2018-07-16 21:18:33 +0200

    Styles init

---
+++ src/App/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    primary: '#EF3B10',
+    secondary: '#82C7F4',
+  },
+};
+
+export default theme;
---
+++ src/scenes/Home/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Box } from 'rebass';
+
+const Home = () => <Box bg="primary" color="white">Hello World</Box>;
+
+export default Home;
