5256f0802224aed135d4c60d80f4e82d93d59666
José Manuel Lucas <josemanuel@jmlweb.es>
2018-07-19 21:19:46 +0200

    Generar ID

--- src/store/archive/actions.js
+++ src/store/archive/actions.js
@@ -1,6 +1,11 @@
+import shortid from 'shortid';
+
 export const ARCHIVE_ADD = 'ARCHIVE/ADD';
 
 export const archiveAdd = payload => ({
   type: ARCHIVE_ADD,
-  payload,
+  payload: {
+    ...payload,
+    id: shortid.generate(),
+  },
 });
--- src/store/archive/reducer.js
+++ src/store/archive/reducer.js
@@ -4,7 +4,6 @@ import initialState from './initialState';
 const archiveReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ARCHIVE_ADD:
-      console.log([...state, payload]);
       return [...state, payload];
     default:
       return state;
